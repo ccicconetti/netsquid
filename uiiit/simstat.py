cbea0acc61bc70c5af481cf3d0cd8daba3b36dd6
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-10-09 18:04:33 +0200

    Update simstat module
    Change behavior of Stat.
    
    Add plot_single() free function.

--- uiiit/simstat.py
+++ uiiit/simstat.py
@@ -1,6 +1,9 @@
 """This module specifies classes that help with the collection of statistics.
 """
 
+import matplotlib
+import matplotlib.pyplot as plt
+
 __all__ = [
     "Stat",
     ]
@@ -14,13 +17,37 @@ class Stat:
     def __repr__(self):
         return ', '.join([f'{k}: {v}' for k, v in self._params.items()])
 
-    def get_count_sum(self, metric):
-        return self._counters[metric][0]
+    def get_sum(self, metric):
+        if metric in self._counters:
+            return self._counters[metric][0]
+        elif metric in self._data:
+            return sum(self._data[metric])
+        raise KeyError(f'Unknown metric {metric}')
 
-    def get_count_avg(self, metric):
-        return self._counters[metric][0] / self._counters[metric][1]
+    def get_count(self, metric):
+        if metric in self._counters:
+            return self._counters[metric][1]
+        elif metric in self._data:
+            return len(self._data[metric])
+        raise KeyError(f'Unknown metric {metric}')
+
+    def get_avg(self, metric):
+        if metric in self._counters:
+            return self._counters[metric][0] / self._counters[metric][1]
+        elif metric in self._data:
+            return sum(self._data[metric]) / len(self._data[metric])
+        raise KeyError(f'Unknown metric {metric}')
+
+    def get_all(self, metric):
+        if metric in self._counters:
+            return self._counters[metric][0]
+        elif metric in self._data:
+            return self._data[metric]
+        raise KeyError(f'Unknown metric {metric}')
 
     def count(self, metric, value):
+        if metric in self._data:
+            raise KeyError(f'Metric {metric} already used')
         if metric not in self._counters:
             self._counters[metric] = [0, 0]
         record = self._counters[metric]
@@ -28,9 +55,23 @@ class Stat:
         record[1] += 1
 
     def add(self, metric, value):
+        if metric in self._counters:
+            raise KeyError(f'Metric {metric} already used')
         if metric not in self._data:
             self._data[metric] = []
         self._data[metric].append(value)
 
-    def get_add(self, metric):
-        return self._data[metric]
+def plot_single(x_values, x_label, stats, metric, func, block=True):
+    if len(x_values) != len(stats):
+        raise ValueError('Inconsistent sizes')
+
+    _, ax = plt.subplots()
+    y_values = [] 
+    for stat in stats:
+        y_values.append(func(stat, metric))
+    ax.plot(x_values, y_values)
+
+    ax.set(xlabel=x_label, ylabel=metric)
+    ax.grid()
+
+    plt.show(block=block)
--- uiiit/test_stat.py
+++ uiiit/test_stat.py
@@ -11,6 +11,21 @@ class TestStat(unittest.TestCase):
 
         self.assertEqual("par1: 1, par2: 0.5, par3: simple", str(stat))
 
+    def test_double_definition(self):
+        stat = Stat()
+
+        stat.count("m1", 1)
+        stat.count("m1", 0.5)
+
+        with self.assertRaises(KeyError):
+            stat.add("m1", 1)
+
+        stat.add("m2", 1)
+        stat.add("m2", 0.5)
+
+        with self.assertRaises(KeyError):
+            stat.count("m2", 1)
+
     def test_counters(self):
         stat = Stat()
 
@@ -19,14 +34,16 @@ class TestStat(unittest.TestCase):
         stat.count("m1", 1)
         stat.count("m1", 0.5)
 
-        self.assertEqual(5, stat.get_count_sum("m1"))
-        self.assertEqual(5/4, stat.get_count_avg("m1"))
+        self.assertEqual(5, stat.get_sum("m1"))
+        self.assertEqual(5, stat.get_all("m1"))
+        self.assertEqual(4, stat.get_count("m1"))
+        self.assertEqual(5/4, stat.get_avg("m1"))
 
         with self.assertRaises(KeyError):
-            stat.get_count_sum("m2")
+            stat.get_sum("m2")
 
         with self.assertRaises(KeyError):
-            stat.get_count_avg("m2")
+            stat.get_avg("m2")
 
     def test_add(self):
         stat = Stat()
@@ -35,10 +52,14 @@ class TestStat(unittest.TestCase):
         for v in values:
             stat.add("m1", v)
 
-        self.assertEqual(values, stat.get_add("m1"))
+        self.assertEqual(2, stat.get_sum("m1"))
+        self.assertEqual(4, stat.get_count("m1"))
+        self.assertEqual(0.5, stat.get_avg("m1"))
+
+        self.assertEqual(values, stat.get_all("m1"))
 
         with self.assertRaises(KeyError):
-            stat.get_add("m2")
+            stat.get_all("m2")
 
 if __name__ == '__main__':
     unittest.main()
