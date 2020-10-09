47bde894d45259bf034b689753110ebecf5160d8
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-10-09 17:07:03 +0200

    Continue working on first_test.py

--- experiments/first_test.py
+++ experiments/first_test.py
@@ -8,10 +8,11 @@ import pandas
 
 import pydynaa
 import netsquid as ns
+from netsquid.qubits import ketstates as ks
 from netsquid.protocols import LocalProtocol, NodeProtocol, Protocol, Signals
 from netsquid.components import Message, QuantumProgram
 from netsquid.components.models.qerrormodels import FibreLossModel
-from netsquid.components.instructions import INSTR_MEASURE_BELL
+from netsquid.components.instructions import INSTR_MEASURE_BELL, INSTR_X, INSTR_Z
 from uiiit.qnetwork import QNetworkUniform
 from uiiit.qrepeater import QRepeater
 from uiiit.topology import Topology
@@ -20,6 +21,42 @@ from uiiit.traffic import SinglePairConstantApplication
 __all__ = []
 
 class SwapProtocol(NodeProtocol):
+    class PathInfo:
+        def __init__(self):
+            self.x_corr = 0
+            self.z_corr = 0
+            self.counter = 0
+
+        def incr(self, x_corr, z_corr):
+            self.counter += 1
+            self.x_corr += x_corr
+            self.z_corr += z_corr
+
+    class SwapProgram(QuantumProgram):
+        """Quantum processor program that measures two qubits."""
+        default_num_qubits = 2
+
+        def program(self):
+            q1, q2 = self.get_qubit_indices(2)
+            self.apply(INSTR_MEASURE_BELL, [q1, q2], output_key="m", inplace=False)
+            yield self.run()
+
+    class CorrectProgram(QuantumProgram):
+        """Quantum processor program that applies all swap corrections."""
+        default_num_qubits = 1
+
+        def set_corrections(self, x_corr, z_corr):
+            self.x_corr = x_corr % 2
+            self.z_corr = z_corr % 2
+
+        def program(self):
+            q1, = self.get_qubit_indices(1)
+            if self.x_corr == 1:
+                self.apply(INSTR_X, q1)
+            if self.z_corr == 1:
+                self.apply(INSTR_Z, q1)
+            yield self.run()
+
     """Perform Swap on a repeater node.
 
     Parameters
@@ -38,59 +75,145 @@ class SwapProtocol(NodeProtocol):
         super().__init__(node, name)
         self._oracle = oracle
         self._qmem = self.node.qmemory
-        self._program = QuantumProgram(num_qubits=2)
-        q1, q2 = self._program.get_qubit_indices(num_qubits=2)
-        self._program.apply(INSTR_MEASURE_BELL, [q1, q2], output_key="m", inplace=False)
 
-        port_names = ' '.join([x for x in self._qmem.ports])
-        logging.info(f"creating SwapProtocol on node {node.name}, ports: {port_names}")
+        # Swap quantum program
+        self._swap_program = SwapProtocol.SwapProgram()
+        # self._swap_program = QuantumProgram(num_qubits=2)
+        # q1, q2 = self._swap_program.get_qubit_indices(num_qubits=2)
+        # self._swap_program.apply(INSTR_MEASURE_BELL, [q1, q2], output_key="m", inplace=False)
+
+        # Correct quantum program
+        self._correct_program = SwapProtocol.CorrectProgram()
+
+        self._rx_messages = dict()
+
+        # Discover classical channel ports
+        self._cport_names = [x for x in self.node.ports]
+        for port in self._cport_names:
+            if 'ccon' not in port:
+                self._cport_names.remove(port)
+
+        qport_names = ' '.join([x for x in self._qmem.ports])
+        cport_names = ' '.join([x for x in self._cport_names])
+        logging.info(f"creating SwapProtocol on node {node.name}, qports: {qport_names}, cports: {cport_names}")
 
     def run(self):
         while True:
-            # Wait for the qubits to arrive
-            event = None
+            # Create an event triggered when ALL the memory ports
+            # receive a qubit: note that also lost qubits will trigger
+            # such an event notification
+            qevent = None
             for port in self._qmem.ports:
                 if port in ["qin", "qout"]:
                     continue
-                if event is None:
-                    event = self.await_port_input(self._qmem.ports[port])
+                if qevent is None:
+                    qevent = self.await_port_input(self._qmem.ports[port])
                 else:
-                    event &= self.await_port_input(self._qmem.ports[port])
-            yield event
+                    qevent &= self.await_port_input(self._qmem.ports[port])
 
-            logging.debug((f"{ns.sim_time():.1f}: {self._qmem.name} "
-                           f"{self._qmem.num_used_positions}/{self._qmem.num_positions} "
-                           f"received (empty: {self._qmem.unused_positions})"))
+            # Create an event triggered when ANY of the classical channel
+            # connections receives a message
+            cevent = None
+            for port in self._cport_names:
+                if cevent is None:
+                    cevent = self.await_port_input(self.node.ports[port])
+                else:
+                    cevent |= self.await_port_input(self.node.ports[port])
+                    
+            # Wait until any of the two events happen
+            expression = yield qevent | cevent
 
-            positions = []
-            for pos in range(self._qmem.num_positions):
-                if pos not in self._qmem.unused_positions:
-                    positions.append(pos)
-            self._oracle.link_good(self.node.name, positions)
+            if expression.first_term.value:
+                # Qubits received on all the memory ports
+                logging.debug((f"{ns.sim_time():.1f}: {self._qmem.name} "
+                            f"{self._qmem.num_used_positions}/{self._qmem.num_positions} "
+                            f"received (empty: {self._qmem.unused_positions})"))
 
-            # Wait for the oracle to take its decisions
-            yield self.await_signal(self._oracle, Signals.SUCCESS)
+                positions = []
+                for pos in range(self._qmem.num_positions):
+                    if pos not in self._qmem.unused_positions:
+                        positions.append(pos)
+                self._oracle.link_good(self.node.name, positions)
 
-            # Entangle the memory positions as specified by the oracle
-            if self.node.name in self._oracle.mem_pos:
-                for item in self._oracle.mem_pos[self.node.name]:
-                    pos1, pos2, dst_name = item
-                    logging.debug(f"{ns.sim_time():.1f}: {self.node.name} ready to swap by measuring on {pos1} and {pos2}")
-                    self.node.qmemory.execute_program(self._program, qubit_mapping=[pos1, pos2])
-                    yield self.await_program(self.node.qmemory)
-                    m, = self._program.output["m"]
-                    m1, m2 = self._bsm_op_indices[m]
+                # Wait for the oracle to take its decisions
+                yield self.await_signal(self._oracle, Signals.SUCCESS)
 
-                    # Send result to one of the two parties creating the
-                    # end-to-end entanglement
-                    cchan = f'ccon{self._oracle.channel_id(self.node.name, dst_name)}'
-                    logging.debug(f"{ns.sim_time():.1f}: {self.node.name} sending corrections ({m1}, {m2}) to {dst_name} via {cchan}")
-                    # self.node.ports["ccon_R"].tx_output(Message([m1, m2], path=0, timeslot=self.oracle.timeslot))
+                # Entangle the memory positions as specified by the oracle
+                if self.node.name in self._oracle.mem_pos:
+                    for item in self._oracle.mem_pos[self.node.name]:
+                        pos1, pos2, dst_name, path = item
+                        logging.debug(f"{ns.sim_time():.1f}: {self.node.name} ready to swap by measuring on {pos1} and {pos2}")
+                        self.node.qmemory.execute_program(self._swap_program, qubit_mapping=[pos1, pos2])
+                        yield self.await_program(self.node.qmemory)
+                        m, = self._swap_program.output["m"]
+                        m1, m2 = self._bsm_op_indices[m]
 
-            # Pop all non-empty but unused memory positions
-            # XXX
+                        # Send result to one of the two parties creating the
+                        # end-to-end entanglement
+                        cchan = self._cport_name(dst_name)
+                        logging.debug((f"{ns.sim_time():.1f}: {self.node.name} sending corrections [{m1}, {m2}] "
+                                       f"(path {path}, timeslot {self._oracle.timeslot}) to {dst_name} via {cchan}"))
+                        self.node.ports[cchan].tx_output(
+                            Message([m1, m2], destination=dst_name,
+                            path=path, timeslot=self._oracle.timeslot))
+            else:
+                # Message received from a classical channel
+                for cport_name in self._cport_names:
+                    port = self.node.ports[cport_name]
+                    if not port.input_queue:
+                        continue
 
-class Oracle(Protocol):
+                    msg = port.rx_input()
+                    if msg.meta['destination'] == self.node.name:
+                        # the message reached its final destination: correct qubit
+                        if self._handle_correct_msg(msg):
+                            yield self.await_program(self.node.qmemory, await_done=True, await_fail=True)
+                            logging.debug(f"{ns.sim_time():.1f}: {self.node.name} corrections applied")
+                            self._oracle.success(msg.meta['path'])
+                            self.send_signal(Signals.SUCCESS)
+                            self._rx_messages.clear()
+                    else:
+                        # we must forward the message to its next hop
+                        self._forward(msg, dst_name)
+
+    def _handle_correct_msg(self, msg):
+        """Handle a new incoming correction message `msg`. Return if executed."""
+
+        path = msg.meta['path']
+        if path not in self._rx_messages:
+            self._rx_messages[path] = SwapProtocol.PathInfo()
+
+        path_info = self._rx_messages[path]
+        path_length = self._oracle.path[path][4]
+        mem_pos = self._oracle.path[path][3]
+
+        m0, m1 = msg.items
+        path_info.incr(m1, m0)
+        
+        if path_info.counter == path_length:
+            if path_info.x_corr or path_info.z_corr:
+                self._correct_program.set_corrections(path_info.x_corr, path_info.z_corr)
+                logging.debug((f"{ns.sim_time():.1f}: {self.node.name} ready to apply corrections "
+                               f"for path {path} to qubit {mem_pos} (status {str(self.node.qmemory.status)})"))
+                self.node.qmemory.execute_program(self._correct_program, qubit_mapping=[mem_pos])
+                return True
+        
+        return False
+
+    def _forward(self, msg, dst_name):
+        """Forward the given message `msg` to the node `dst_name`."""
+
+        cchan = self._cport_name(dst_name)
+        logging.debug((f"{ns.sim_time():.1f}: {self.node.name} forwarding message (path {msg.meta['path']}, "
+                        f"timeslot {msg.meta['timeslot']}) to {dst_name} via {cchan}"))
+        self.node.ports[cchan].tx_output(msg)
+
+    def _cport_name(self, dst_name):
+        """Return the port name to reach the given destination"""
+
+        return f'ccon{self._oracle.channel_id(self.node.name, dst_name)}'         
+
+class Oracle(Protocol):    
     """Network oracle: knows everything, can communicate at zero delay.
 
     Parameters
@@ -107,13 +230,22 @@ class Oracle(Protocol):
     ----------
     timeslot : int
         The current timeslot, counting from 0.
-    mem_pos : dist
+    mem_pos : dict
         A dictionary containing the outcome of the routing algorithm.
         For each element, the key is the name of the node that has to swap
-        two qubits in its internal memory; the value is a list of tuples, where
+        two qubits in its internal memory; the value is a list of tuples, where:
         the first two items hold the identifiers of the memory positions that
-        have to be swapped and the third item is the name of the node to which
-        the corrections have to be sent.
+        have to be swapped; the third item is the name of the node to which
+        the corrections have to be sent; the fourth item is an identifier of
+        the qubit in this timeslot. This structure is overwritten at every new
+        timeslot.
+    path : dict
+        A dictionary containing for each path (in the key) a tuple with the
+        following data: in the first two items the names of the alice and bob
+        nodes; in the third item the memory position on alice of the
+        qubit teleported; in the fourth item the memory position on bob of the
+        qubit teleported; in the fifth item the number of swaps along the path.
+        This structure is overwritten at every new timeslot.
 
     """
     def __init__(self, network, topology, app):
@@ -128,6 +260,9 @@ class Oracle(Protocol):
 
         self.timeslot = 0
         self.mem_pos = dict()
+        self.path = dict()
+
+        self.num_successful = 0
 
         print(f"Create Oracle for network {network.name}, app {app.name}, nodes: {topology.node_names}")
 
@@ -159,6 +294,7 @@ class Oracle(Protocol):
 
         # remove previous entanglement data structure
         self.mem_pos.clear()
+        self.path.clear()
 
         # Create a new graph with only the edges where entanglement has succeeded
         graph_uni = Topology("edges", edges=self._edges)
@@ -172,15 +308,35 @@ class Oracle(Protocol):
         pairs = self._app.get_pairs(self.timeslot)
 
         assert len(pairs) == 1
-        alice = graph_bi.get_id_by_name(pairs[0][0])
-        bob = graph_bi.get_id_by_name(pairs[0][1])
-        prev, _ = graph_bi.spt(alice)
-        if prev[bob] is None:
-            logging.debug(f"timeslot #{self.timeslot}, no way to create e2e entanglement between {pairs[0][0]} and {pairs[0][1]}")
+        alice_name = pairs[0][0]
+        bob_name = pairs[0][1]
+        alice = graph_bi.get_id_by_name(alice_name) if alice_name in graph_bi.node_names else None
+        bob = graph_bi.get_id_by_name(bob_name) if bob_name in graph_bi.node_names else None
+
+        # Search the path from bob to alice, but only if both are still in
+        # the reduced graph
+        prev = None
+        if alice is not None and bob is not None:
+            prev, _ = graph_bi.spt(alice)
+        
+        if prev is None or prev[bob] is None:
+            logging.debug(f"timeslot #{self.timeslot}, no way to create e2e entanglement between {alice_name} and {bob_name}")
 
         else:
             # there is a path between alice and bob
+            assert alice is not None
+            assert bob is not None
+
             swap_nodes = Topology.traversing(prev, bob, alice)
+            alice_nxt = swap_nodes[-1] if swap_nodes else bob
+            bob_prv = swap_nodes[0] if swap_nodes else alice
+            self.path[0] = [
+                alice_name,
+                bob_name,
+                self._topology.incoming_id(alice, alice_nxt),
+                self._topology.incoming_id(bob, bob_prv),
+                self._topology.distance(alice, bob) - 1,
+            ]
             # logging.debug(f"timeslot #{self.timeslot}, path {bob}, {', '.join([str(x) for x in swap_nodes])}, {alice}")
 
             for i in range(len(swap_nodes)):
@@ -189,7 +345,7 @@ class Oracle(Protocol):
                 nxt = alice if i == (len(swap_nodes)-1) else swap_nodes[i+1]
                 prv_pos = self._topology.incoming_id(cur, prv)
                 nxt_pos = self._topology.incoming_id(cur, nxt)
-                logging.debug((f"timeslot #{self.timeslot}, e2e entanglement between {pairs[0][0]} and {pairs[0][1]}: "
+                logging.debug((f"timeslot #{self.timeslot}, e2e entanglement between {alice_name} and {bob_name}: "
                                f"on node {cur} entangle node {prv} (mem pos {prv_pos}) and node {nxt} (mem pos {nxt_pos})"))
 
                 cur_name = self._topology.get_name_by_id(cur)
@@ -197,7 +353,8 @@ class Oracle(Protocol):
                     self.mem_pos[cur_name] = []
                 self.mem_pos[cur_name].append([
                     prv_pos, nxt_pos,
-                    self._topology.get_name_by_id(bob)])
+                    self._topology.get_name_by_id(bob),
+                    0])
 
         # Notify all nodes that they can proceed
         self.send_signal(Signals.SUCCESS)
@@ -233,21 +390,20 @@ class Oracle(Protocol):
         nxt_id = self._topology.next_hop(src_id, dst_id)
         return self._topology.incoming_id(src_id, nxt_id)
         
-    def set_src_node(self, node):
-        logging.debug(f"{ns.sim_time():.1f}: {self.name} {node.name} is the SRC node")
-        self.src_node = node.name
+    def success(self, path_id):
+        """The path `path_id` in this timeslot is successful."""
 
-    def set_dst_node(self, node):
-        logging.debug(f"{ns.sim_time():.1f}: {self.name} {node.name} is the DST node")
-        self.dst_node = node.name
-
-    def success(self):
         self.num_successful += 1
 
-    def path_length(self, path):
-        if path != 0:
-            raise Exception(f"Unknown path {path}")
-        return len(self.node_names) - 2
+        # Measure fidelity
+        path = self.path[path_id]
+        qubit_a, = self._network.nodes[path[0]].qmemory.peek([path[2]])
+        qubit_b, = self._network.nodes[path[1]].qmemory.peek([path[3]])
+        fidelity = ns.qubits.fidelity([qubit_a, qubit_b], ks.b00, squared=True)
+        logging.debug((f"timeslot #{self.timeslot}, measured fidelity of e2e entanglement "
+                       f"between {path[0]}:{path[2]} and {path[1]}:{path[3]}: {fidelity:.3f}"))
+
+        
 
 def run_simulation(num_nodes, node_distance, timeslots, seed):
     logging.info(f"starting simulation #{seed}: num_nodes = {num_nodes}, distance = {node_distance} km, messages = {timeslots}")
@@ -257,7 +413,7 @@ def run_simulation(num_nodes, node_distance, timeslots, seed):
     est_runtime = (0.5 + 2 * num_nodes - 2) * node_distance * 5e3
     logging.debug(f"estimated maximum end-to-end delay = {est_runtime} ns")
 
-    p_loss_init = 0.1
+    p_loss_init   = 0.1
     p_loss_length = 0.1
     network_factory = QNetworkUniform(
         node_distance=node_distance,
@@ -265,7 +421,12 @@ def run_simulation(num_nodes, node_distance, timeslots, seed):
         source_frequency=1e9 / est_runtime,
         qerr_model=FibreLossModel(p_loss_init=p_loss_init, p_loss_length=p_loss_length))
 
-    qrepeater_factory = QRepeater(1e6, 1e6, 1)
+    dephase_rate  = 1000 # Hz
+    depol_rate    = 1000 # Hz
+    gate_duration = 2    # ns
+    qrepeater_factory = QRepeater(dephase_rate=dephase_rate,
+                                  depol_rate=depol_rate,
+                                  gate_duration=gate_duration)
 
     topology = Topology("grid", size=num_nodes)
 
@@ -311,8 +472,8 @@ if __name__ == "__main__":
     ns.set_qstate_formalism(ns.QFormalism.DM)
 
     num_nodes=3
-    distance = 6.25 # km
-    timeslots = 2
+    distance = 5 # km
+    timeslots = 3
     seed = 42
 
     df = run_simulation(num_nodes=num_nodes,
