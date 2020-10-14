1229f48ed018a9fb0501597dce0834a2c2cd743d
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-10-14 16:19:25 +0200

    Add traffic.SingleRandomPairs
    Also refactor a bit the SinglePairConstantApplication by creating a new
    superclass SingleApplication for applications
    returning a single pair of nodes and a constant value for the number
    of minimum qubits required.

--- uiiit/test_traffic.py
+++ uiiit/test_traffic.py
@@ -3,11 +3,11 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 import unittest
-from traffic import SinglePairConstantApplication
+from traffic import SinglePairConstantApplication, SingleRandomPairs
 
 class TestApplication(unittest.TestCase):
     def test_single_pair_constant_application(self):
-        app = SinglePairConstantApplication("App", "alice", "bob")
+        app = SinglePairConstantApplication("App", "alice", "bob", 1)
 
         self.assertEqual("App", app.name)
 
@@ -15,6 +15,58 @@ class TestApplication(unittest.TestCase):
             pairs = app.get_pairs(timeslot)
             self.assertEqual(1, len(pairs))
             self.assertEqual(("alice", "bob", 1), pairs[0])
+
+        app2 = SinglePairConstantApplication("", "alice", "bob", 2)
+    
+        self.assertEqual([("alice", "bob", 2)], app2.get_pairs(0))
+
+        with self.assertRaises(ValueError):
+            SinglePairConstantApplication("", "alice", "alice", 2)
+
+        with self.assertRaises(ValueError):
+            SinglePairConstantApplication("", "alice", "bob", 0)
+
+        with self.assertRaises(ValueError):
+            SinglePairConstantApplication("", "alice", "bob", -1)
+
+    def test_single_random_pairs(self):
+        with self.assertRaises(ValueError):
+            SingleRandomPairs("App", [], 1)
+
+        with self.assertRaises(ValueError):
+            SingleRandomPairs("App", ["alice"], 1)
+
+        with self.assertRaises(ValueError):
+            SingleRandomPairs("App", ["alice", "bob"], 0)
+
+        with self.assertRaises(ValueError):
+            SingleRandomPairs("App", ["alice", "bob"], -1)
+
+        app = SingleRandomPairs("App", ["alice", "bob"], 7)
+
+        self.assertEqual("App", app.name)
+
+        for timeslot in range(10):
+            pairs = app.get_pairs(timeslot)
+            self.assertEqual(1, len(pairs))
+            pair = pairs[0]
+            self.assertTrue(pair[0] in ["alice", "bob"])
+            if pair[0] == "alice":
+                self.assertEqual("bob", pair[1])
+            else:
+                self.assertEqual("alice", pair[1])
+            self.assertEqual(7, pair[2])
+
+        node_names = set([f'{x}' for x in range(10)])
+
+        app2 = SingleRandomPairs("", node_names, 1)
+
+        found_names = set()
+        for timeslot in range(100):
+            pair = app2.get_pairs(timeslot)[0]
+            found_names.add(pair[0])
+
+        self.assertEqual(node_names, found_names)
 
 if __name__ == '__main__':
     unittest.main()
--- uiiit/traffic.py
+++ uiiit/traffic.py
@@ -1,6 +1,8 @@
 """This module specifies classes that model an application traffic pattern.
 """
 
+import random
+
 __all__ = [
     "Application",
     "SinglePairConstantApplication",
@@ -40,8 +42,37 @@ class Application:
         """
         raise NotImplementedError("Class Application should be inherited")
 
-class SinglePairConstantApplication(Application):
-    """Return always the same pair.
+class SingleApplication(Application):
+    """Abstract class to be used by applications returning a single pair.
+
+    Also, the number of qubits is always the same, as set in the ctor.
+
+    Parameters
+    ----------
+    name : str
+        A name to identify this application.
+    min_qubits : int
+        The minimum number of qubits required.
+
+    """
+    def __init__(self, name, min_qubits):
+        super().__init__(name)
+
+        if min_qubits <= 0:
+            raise ValueError("Cannot have zero or negative number of qubits specified")
+
+        self._min_qubits = min_qubits
+
+    def get_pairs(self, timeslot):
+        # timeslot is unused
+        pair = self._get_single_pair()
+        return [(pair[0], pair[1], self._min_qubits)]
+
+    def _get_single_pair(self):
+        raise NotImplementedError("Class SingleApplication should be inherited")
+
+class SinglePairConstantApplication(SingleApplication):
+    """Return always the same pair, all with the same minimum number of qubits.
 
     Parameters
     ----------
@@ -51,14 +82,45 @@ class SinglePairConstantApplication(Application):
         The name of the first end-point
     bob : str
         The name of the second end-point
+    min_qubits : int
+        The minimum number of qubits required.
     
     """
 
-    def __init__(self, name, alice, bob):
-        super().__init__(name)
+    def __init__(self, name, alice, bob, min_qubits):
+        super().__init__(name, min_qubits)
+
+        if alice == bob:
+            raise ValueError(f"Cannot use the same name in SinglePairConstantApplication: {alice}")
 
         self._alice = alice
         self._bob = bob
 
-    def get_pairs(self, timeslot):
-        return [(self._alice, self._bob, 1)]
+    def _get_single_pair(self):
+        return [self._alice, self._bob]
+
+class SingleRandomPairs(SingleApplication):
+    """Return a random pair from a set, all with the same minimum number of qubits.
+
+    Parameters
+    ----------
+    name : str 
+        A name to identify this application.
+    node_names : iterable
+        The possible names from which to extract the pair
+    min_qubits : int
+        The minimum number of qubits required.
+    
+    """
+
+    def __init__(self, name, node_names, min_qubits):
+        super().__init__(name, min_qubits)
+
+        self._node_names = set(node_names)
+
+        if len(self._node_names) <= 1:
+            raise ValueError(("Invalid cardinality of set of names passed to "
+                              f"SingleRandomPairs: {len(self._node_names)}"))
+
+    def _get_single_pair(self):
+        return random.sample(self._node_names, 2)
