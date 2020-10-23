26030df045ceadd4a8a1d9833ad97208a42b00eb
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2020-10-23 13:39:29 +0200

    Add SocketCollector and SocketSender to uiiit.utils
    Classes to collect/send serialized Python objects over TCP

--- uiiit/test_utils.py
+++ uiiit/test_utils.py
@@ -3,56 +3,144 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 import unittest
-import os
+
+import logging
 import multiprocessing
+import os
+import pickle
+import socket
 from time import sleep
 from random import uniform
 
-from utils import ParallerRunner
+from utils import ParallerRunner, SocketCollector, SocketSender
 
 def my_func(arg):
-  sleep(uniform(0, 0.2))
-  return arg[0] + arg[1]
+    sleep(uniform(0, 0.2))
+    return arg[0] + arg[1]
 
 def func_get_pid(arg):
-  sleep(1)
-  return os.getpid()
+    sleep(1)
+    return os.getpid()
 
 class TestUtils(unittest.TestCase):
-  def test_run_operations(self):
-    args = []
-    expected = set()
-    for i in range(50):
-      args.append([i, i+1])
-      expected.add(2 * i + 1)
-    ret = ParallerRunner.run(10, my_func, args)
-    
-    self.assertEqual(expected, set(ret))
+    def test_run_operations(self):
+        args = []
+        expected = set()
+        for i in range(50):
+            args.append([i, i+1])
+            expected.add(2 * i + 1)
+        ret = ParallerRunner.run(10, my_func, args)
 
-  def test_run_getpid(self):
-    nworkers = 10
+        self.assertEqual(expected, set(ret))
 
-    #
-    # num workers > number of tasks
-    #
-    args = [None for _ in range(nworkers // 2)]
-    ret = ParallerRunner.run(nworkers, func_get_pid, args)
+    def test_run_getpid(self):
+        nworkers = 10
 
-    # check that each worker has its own PID
-    self.assertEqual(nworkers // 2, len(set(ret)))
+        #
+        # num workers > number of tasks
+        #
+        args = [None for _ in range(nworkers // 2)]
+        ret = ParallerRunner.run(nworkers, func_get_pid, args)
 
-    #
-    # num workers < number of tasks
-    #
-    args = [None for _ in range(nworkers * 2)]
-    ret = ParallerRunner.run(nworkers, func_get_pid, args)
+        # check that each worker has its own PID
+        self.assertEqual(nworkers // 2, len(set(ret)))
 
-    # check that each task has been executed
-    self.assertEqual(nworkers * 2, len(ret))
+        #
+        # num workers < number of tasks
+        #
+        args = [None for _ in range(nworkers * 2)]
+        ret = ParallerRunner.run(nworkers, func_get_pid, args)
 
-    # check that PIDs have been recycled
-    # (might not be true on all operating systems)
-    self.assertLessEqual(nworkers, len(set(ret)))
+        # check that each task has been executed
+        self.assertEqual(nworkers * 2, len(ret))
+
+        # check that PIDs have been recycled
+        # (might not be true on all operating systems)
+        self.assertLessEqual(nworkers, len(set(ret)))
+
+    @unittest.skip
+    def test_socket_collector(self):
+        logging.basicConfig(level=logging.INFO)
+        collector = SocketCollector('localhost', 21001)
+
+        num_expected = 10
+        collector.collect(num_expected)
+
+    @unittest.skip
+    def test_socket_sender(self):
+        logging.basicConfig(level=logging.INFO)
+        sender = SocketSender('localhost', 21001)
+
+        big_msg = [x for x in range(2**20)]
+        
+        for i in range(10):
+            logging.info(f'Sending message #{i}')
+            sender.send(big_msg)
+
+    @unittest.skip
+    def test_socket_server(self):
+        expected = [x for x in range(100000)]
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            s.bind(('localhost', 21001))
+            s.listen()
+
+            while True:
+                print(f"Waiting for a connection")
+                clientsocket, address = s.accept()
+                print(f"Connection from {address} has been established")
+
+                while True:
+
+                    hdr = clientsocket.recv(4)
+                    if not hdr:
+                        clientsocket.close()
+                        break
+                    
+                    msg_len = int.from_bytes(hdr, byteorder='big', signed=False)
+                    print(f'Received header of a message with size {msg_len}')
+
+                    buf = b''
+                    total = 0
+
+                    while total != msg_len:
+                        msg = clientsocket.recv(msg_len - total)
+                        print(f'Received message chunk of size {len(msg)}')
+                        buf += msg
+                        total += len(msg)
+                    
+                    assert len(buf) == msg_len
+                    print(f'Full message of size {  msg_len} received')
+
+                    obj = pickle.loads(buf)
+                    assert obj == expected
+
+        finally:
+            s.close()
+
+    @unittest.skip
+    def test_socket_client(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 21001))
+
+        big_msg = [x for x in range(100000)]
+
+        try:
+            while True:
+
+                big_msg_ser = pickle.dumps(big_msg)
+
+                hdr = int.to_bytes(len(big_msg_ser), 4, byteorder='big', signed=False)
+
+                print(f"sending message of size 4+{len(big_msg_ser)} bytes")
+                sent = s.send(hdr + big_msg_ser)
+                print(f"send {sent} bytes")
+
+                sleep(1)
+
+        finally:
+            s.close()
 
 if __name__ == '__main__':
     unittest.main()
--- uiiit/utils.py
+++ uiiit/utils.py
@@ -1,72 +1,196 @@
 """This module includes some general-purpose utility functions and classes
 """
 
+import socket
+import pickle
+import logging
 from multiprocessing import Process, Queue
 from queue import Empty
 
 __all__ = [
-    "ParallerRunner"
+    "ParallerRunner",
+    "SocketCollector",
     ]
 
-class ParallerRunner:
-  @staticmethod
-  def _sub_func(qin, qout, func):
-    """Single worker called by `run_parallel`."""
-
-    while True:
-      try:
-        args = qin.get_nowait()
-      except Empty:
-        return
-
-      qout.put(func(args))
-
-  @staticmethod
-  def run(nworkers, func, args):
-    """Run a given function in parallel and return the list of their return values.
+class SocketSender:
+    """Sends a pickle-serialized object via TCP socket.
 
     Parameters
     ----------
-      nworkers : int
-        The number of workers to spawn.
-      func : lambda
-        The function to call.
-      args : list
-        The list of arguments. The size of this list is the same as the number
-        of executions of the function.
-
-      Returns
-      -------
-      A list of items, one for each function invoked.
-      
-      Raises
-      ------
-      ValueError
-        If the number of workers is smaller than 1.
-
-      """
-
-    if nworkers < 1:
-      raise ValueError(f"Invalid number of workers: {nworkers}")
-
-    qin = Queue()
-    for arg in args:
-      qin.put(arg)
-    qout = Queue()
-
-    # assert qin.qsize() == len(args)
-
-    processes = []
-    for _ in range(nworkers):
-      p = Process(target=ParallerRunner._sub_func, args=(qin, qout, func))
-      p.start()
-      processes.append(p)
+    address : str
+        The address of the server.
+    port : int
+        The TCP server's port.
     
-    for p in processes:
-      p.join()
+    """
+    def __init__(self, address, port):
+        self._address = address
+        self._port = port
 
-    ret = []
-    while not qout.empty():
-      ret.append(qout.get_nowait())
+    def send(self, obj):
+        """Send the given object to the configured server.
 
-    return ret
+        Note that the socket is opened in this call and closed immediately
+        after the object has been sent.
+
+        Parameters
+        ----------
+        obj
+            The object to be sent. Anything pickle-serializable is OK, though
+            its maximum (serialized) size must fit in an unsigned 32-bit int.
+        
+        Raises
+        ------
+        ValueError
+            If the object is too big.
+        """
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self._address, self._port))
+
+        msg = pickle.dumps(obj)
+        if len(msg) >= 2**32:
+            raise ValueError(f'The serialization of the object is too big ({len(msg)} bytes)')
+
+        try:
+            hdr = int.to_bytes(len(msg), 4, byteorder='big', signed=False)
+
+            logging.info(f'Sending object of size ({len(msg)}) to {self._address}:{self._port}')
+            sent = s.send(hdr + msg)
+            assert sent == (4 + len(msg))
+
+        finally:
+            s.close()
+
+class SocketCollector:
+    """Collects pickle-serialized objects sent via a TCP socket.
+
+    Parameters
+    ----------
+    address : str
+        The address where to bind the listening socket.
+    port : int
+        The port where to bind the listing socket.
+    
+    """
+
+    def __init__(self, address, port):
+        self._address = address
+        self._port = port
+
+    def collect(self, expected):
+        """Collect a given number of objects, then quit.
+
+        Parameters
+        ----------
+        expected : int
+            The number of objects expected to be collected.
+
+        """
+
+        ret = [] # the list of objects returned
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.bind((self._address, self._port))
+            s.listen()
+            logging.info((f"Listening on {self._address}:{self._port}, "
+                          f"waiting for {expected} objects"))
+
+            while len(ret) < expected:
+                clientsocket, address = s.accept()
+                logging.info(f"Connection from {address} established")
+
+                try:
+                    while len(ret) < expected:
+                        hdr = clientsocket.recv(4)
+                        if not hdr:
+                            clientsocket.close()
+                            break
+                        
+                        msg_len = int.from_bytes(hdr, byteorder='big', signed=False)
+
+                        buf = b''
+                        total = 0
+
+                        while total != msg_len:
+                            msg = clientsocket.recv(msg_len - total)
+                            buf += msg
+                            total += len(msg)
+                        
+                        assert len(buf) == msg_len
+
+                        logging.info(f"object received, {expected - len(ret) - 1} to go")
+
+                        ret.append(pickle.loads(buf))
+                
+                finally:
+                    clientsocket.close()
+
+        finally:
+            s.close()
+        
+        return ret
+
+class ParallerRunner:
+    @staticmethod
+    def _sub_func(qin, qout, func):
+        """Single worker called by `run_parallel`."""
+
+        while True:
+            try:
+                args = qin.get_nowait()
+            except Empty:
+                return
+
+            qout.put(func(args))
+
+    @staticmethod
+    def run(nworkers, func, args):
+        """Run a given function in parallel and return the list of their return values.
+
+        Parameters
+        ----------
+        nworkers : int
+            The number of workers to spawn.
+        func : lambda
+            The function to call.
+        args : list
+            The list of arguments. The size of this list is the same as the number
+            of executions of the function.
+
+        Returns
+        -------
+        A list of items, one for each function invoked.
+        
+        Raises
+        ------
+        ValueError
+            If the number of workers is smaller than 1.
+
+        """
+
+        if nworkers < 1:
+            raise ValueError(f"Invalid number of workers: {nworkers}")
+
+        qin = Queue()
+        for arg in args:
+            qin.put(arg)
+        qout = Queue()
+
+        # assert qin.qsize() == len(args)
+
+        processes = []
+        for _ in range(nworkers):
+            p = Process(target=ParallerRunner._sub_func, args=(qin, qout, func))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        ret = []
+        while not qout.empty():
+            ret.append(qout.get_nowait())
+
+        return ret
