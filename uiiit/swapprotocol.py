02d72a90e13bc8c50343936649269c6463b7fdc4
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-10-28 01:07:48 +0100

    Fix issue in SwapProtocol
    Now possible to handle incoming messages while correcting qubits

--- uiiit/swapprotocol.py
+++ uiiit/swapprotocol.py
@@ -95,6 +95,14 @@ class SwapProtocol(NodeProtocol):
         logging.debug(f"creating SwapProtocol on node {node.name}, qports: {qport_names}, cports: {cport_names}")
 
     def run(self):
+        qports = []
+        for port in self._qmem.ports:
+            qports.append(self._qmem.ports[port])
+
+        qprog_exec    = False
+        qprog_path_id = None
+        qprog_queue   = []
+
         while True:
             # Create an event triggered when ALL the memory ports
             # receive a qubit: note that also lost qubits will trigger
@@ -116,129 +124,139 @@ class SwapProtocol(NodeProtocol):
                     cevent = self.await_port_input(self.node.ports[port])
                 else:
                     cevent |= self.await_port_input(self.node.ports[port])
-                    
-            # Wait until any of the two events happen
-            expression = yield qevent | cevent
 
-            # print(expression.triggered_events[-1].source)
-
-            if expression.first_term.value:
-                # Qubits received on all the memory ports
-                logging.debug((f"{ns.sim_time():.1f}: {self._qmem.name} "
-                            f"{self._qmem.num_used_positions}/{self._qmem.num_positions} "
-                            f"received (empty: {self._qmem.unused_positions})"))
-
-                # All previously received messages should be entirely consumed
-                assert not self._rx_messages, \
-                    (f'{self.node.name} has non-empty RX messages upon the '
-                     f'beginning of a new timeslot: {self._rx_messages}')
-
-                positions = []
-                for pos in range(self._qmem.num_positions):
-                    if pos not in self._qmem.unused_positions:
-                        positions.append(pos)
-                self._oracle.link_good(self.node.name, positions)
-
-                # Wait for the oracle to take its decisions
-                yield self.await_signal(self._oracle, Signals.SUCCESS)
-
-                # Entangle the memory positions as specified by the oracle
-                if self.node.name in self._oracle.mem_pos:
-                    for item in self._oracle.mem_pos[self.node.name]:
-                        pos1, pos2, dst_name, path = item
-                        logging.debug(f"{ns.sim_time():.1f}: {self.node.name} ready to swap by measuring on {pos1} and {pos2}")
-                        self.node.qmemory.execute_program(self._swap_program, qubit_mapping=[pos1, pos2])
-                        yield self.await_program(self.node.qmemory)
-                        m, = self._swap_program.output["m"]
-                        m1, m2 = self._bsm_op_indices[m]
-
-                        # Send result to one of the two parties creating the
-                        # end-to-end entanglement
-                        cchan = self._cport_name(dst_name)
-                        logging.debug((f"{ns.sim_time():.1f}: {self.node.name} sending corrections [{m1}, {m2}] "
-                                       f"(path {path}, timeslot {self._oracle.timeslot}) to {dst_name} via {cchan}"))
-                        msg =  Message([m1, m2], source=self.node.name, destination=dst_name,
-                                       path=path, timeslot=self._oracle.timeslot)
-                        self.node.ports[cchan].tx_output(msg)
+            # Wait until any of the events happen
+            if qprog_exec:
+                expression = yield qevent | cevent | \
+                    self.await_program(self.node.qmemory,
+                                       await_done=True, await_fail=True)
             else:
-                # Message received from a classical channel
-                for cport_name in self._cport_names:
-                    port = self.node.ports[cport_name]
-                    if not port.input_queue:
-                        continue
+                expression = yield qevent | cevent
 
-                    assert len(port.input_queue) == 1
-                    msg = port.rx_input()
-                    logging.debug(f"{ns.sim_time():.1f}: {self.node.name} received message: {msg}")
-                    if msg.meta['destination'] != self.node.name:
-                        # We must forward the message to its next hop
-                        self._forward(msg)
-                        continue
+            qports_done = False
+            for ev in expression.triggered_events:
+                if not qports_done and ev.source in qports:
+                    qports_done = True
+                    # Qubits received on all the memory ports
+                    logging.debug((f"{ns.sim_time():.1f}: {self._qmem.name} "
+                                f"{self._qmem.num_used_positions}/{self._qmem.num_positions} "
+                                f"received (empty: {self._qmem.unused_positions})"))
 
-                    #
-                    # The message reached its final destination: correct qubit
-                    #
+                    # All previously received messages should be entirely consumed
+                    assert not self._rx_messages, \
+                        (f'{self.node.name} has non-empty RX messages upon the '
+                         f'beginning of a new timeslot: {self._rx_messages}')
 
-                    # From NetSquid documentation:
-                    #
-                    # https://docs.netsquid.org/latest-release/api_components/netsquid.components.channel.html
-                    #
-                    # If multiple items are send onto a channel during
-                    # the same time instance, then all items become part
-                    # of the same message with the same arrival time.
-                    # When getting a message with multiple items, all
-                    # items can be retrieved at once, or individual
-                    # items can be got by their indices.
-                    #
-                    # Thus, we might have to unpack the items at pairs.
-                    #
-                    # NOTE that this way the metadata info has been lost.
-                    #
+                    positions = []
+                    for pos in range(self._qmem.num_positions):
+                        if pos not in self._qmem.unused_positions:
+                            positions.append(pos)
+                    self._oracle.link_good(self.node.name, positions)
 
-                    assert len(msg.items) == 2, \
-                        (f"expecting a Message with exactly 2 items, "
-                         f"got one with {len(msg.items)}")
-                    path_id = msg.meta['path']
+                    # Wait for the oracle to take its decisions
+                    yield self.await_signal(self._oracle, Signals.SUCCESS)
 
-                    if path_id not in self._rx_messages:
-                        self._rx_messages[path_id] = SwapProtocol.PathInfo(self._oracle.timeslot)
+                    # Entangle the memory positions as specified by the oracle
+                    if self.node.name in self._oracle.mem_pos:
+                        for item in self._oracle.mem_pos[self.node.name]:
+                            pos1, pos2, dst_name, path = item
+                            logging.debug(f"{ns.sim_time():.1f}: {self.node.name} ready to swap by measuring on {pos1} and {pos2}")
+                            self.node.qmemory.execute_program(self._swap_program, qubit_mapping=[pos1, pos2])
+                            yield self.await_program(self.node.qmemory)
+                            m, = self._swap_program.output["m"]
+                            m1, m2 = self._bsm_op_indices[m]
 
-                    path = self._rx_messages[path_id]
-                    assert self._oracle.timeslot == path.timeslot
+                            # Send result to one of the two parties creating the
+                            # end-to-end entanglement
+                            cchan = self._cport_name(dst_name)
+                            logging.debug((f"{ns.sim_time():.1f}: {self.node.name} sending corrections [{m1}, {m2}] "
+                                        f"(path {path}, timeslot {self._oracle.timeslot}) to {dst_name} via {cchan}"))
+                            msg =  Message([m1, m2], source=self.node.name, destination=dst_name,
+                                        path=path, timeslot=self._oracle.timeslot)
+                            self.node.ports[cchan].tx_output(msg)
 
-                    m0, m1 = msg.items[0:2]
-                    path.incr(m1, m0)
+                # Check if a QPU operation is complete
+                if ev.source == self.node.qmemory:
+                    assert qprog_path_id is not None
+                    assert qprog_exec == True
 
-                    # Check if all the messages for all the paths terminating
-                    # in this node have been received: if yes, then correct
-                    # all local qubits based on the items received in the
-                    # messages for the respective paths. Otherwise, we wait
-                    # for more messages to arrive.
+                    self._notify_oracle(qprog_path_id)
+                    qprog_path_id = None
+                    while qprog_queue:
+                        qprog_path_id = qprog_queue.pop()
+                        if self._correct(qprog_path_id):
+                            break
+                        else:
+                            self._notify_oracle(qprog_path_id)
+                            qprog_path_id = None
+                    if qprog_path_id is None:
+                        qprog_exec = False
 
-                    all_received = True
-                    paths = []
-                    for cur_path_id, cur_path in self._oracle.path.items():
-                        if cur_path.bob_name == self.node.name:
-                            if cur_path_id not in self._rx_messages or \
-                                self._rx_messages[cur_path_id].counter < cur_path.num_swaps:
-                                all_received = False
-                                break
-                            paths.append(cur_path_id)
+            # Check messages received from a classical channel
+            for cport_name in self._cport_names:
+                port = self.node.ports[cport_name]
+                if not port.input_queue:
+                    continue
 
-                    if all_received:
-                        for cur_path_id in paths:
-                            if self._correct(cur_path_id):
-                                yield self.await_program(
-                                    self.node.qmemory,
-                                    await_done=True, await_fail=True)
+                assert len(port.input_queue) == 1
+                msg = port.rx_input()
+                logging.debug(f"{ns.sim_time():.1f}: {self.node.name} received message: {msg}")
+                if msg.meta['destination'] != self.node.name:
+                    # We must forward the message to its next hop
+                    self._forward(msg)
+                    continue
 
-                            # Notify the Oracle
-                            logging.debug((f"{ns.sim_time():.1f}: {self.node.name} "
-                                           f"path {cur_path_id} corrections applied"))
-                            del self._rx_messages[cur_path_id]
-                            self._oracle.success(cur_path_id)
-                            self.send_signal(Signals.SUCCESS)
-                           
+                #
+                # The message reached its final destination: correct qubit
+                #
+
+                # From NetSquid documentation:
+                #
+                # https://docs.netsquid.org/latest-release/api_components/netsquid.components.channel.html
+                #
+                # If multiple items are send onto a channel during
+                # the same time instance, then all items become part
+                # of the same message with the same arrival time.
+                # When getting a message with multiple items, all
+                # items can be retrieved at once, or individual
+                # items can be got by their indices.
+                #
+                # Thus, we might have to unpack the items at pairs.
+                #
+                # NOTE that this way the metadata info has been lost.
+                #
+
+                assert len(msg.items) == 2, \
+                    (f"expecting a Message with exactly 2 items, "
+                    f"got one with {len(msg.items)}")
+                path_id = msg.meta['path']
+
+                if path_id not in self._rx_messages:
+                    self._rx_messages[path_id] = SwapProtocol.PathInfo(self._oracle.timeslot)
+
+                path = self._rx_messages[path_id]
+                assert self._oracle.timeslot == path.timeslot
+
+                m0, m1 = msg.items[0:2]
+                path.incr(m1, m0)
+
+                if path.counter == self._oracle.path[path_id].num_swaps:
+                    if qprog_exec:
+                        qprog_queue.append(path_id)
+                    else:
+                        if self._correct(path_id):
+                            qprog_exec = True
+                            qprog_path_id = path_id
+                        else:
+                            self._notify_oracle(path_id)
+
+    def _notify_oracle(self, path_id):
+        logging.debug((f"{ns.sim_time():.1f}: {self.node.name} "
+                       f"path {path_id} corrections applied"))
+        del self._rx_messages[path_id]
+        self._oracle.success(path_id)
+        self.send_signal(Signals.SUCCESS)
+
     def _correct(self, path_id):
         """Execute the correction program for a given path.
         
